726ee61504700aa9f615635ee7a0ba34cb091e72
supdi <supriya@idyllic.co>
2018-01-16 15:39:20 +0530

    Project Setup

---
+++ src/test/java/Diesl/Signup.java
@@ -0,0 +1,15 @@
+
+package Diesl;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Signup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://dieslidyllic.club/");
+	}
+
+}
